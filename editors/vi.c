ziuziakowska/busybox

commit e3fa0b3d1ca7733661e1e79f725b0d0e9408c38a

Fix pointer handling in busybox's vi
The upstream code uses realloc to increase an existing memory
region and then adds the pointer difference (bias) between the
old and the new pointer the old pointer to obtain the pointer
in the reallocated region. With CHERI this fails because the
resulting pointer is out of bounds with respect to the origin
allocation. The situation is complicated by the fact that the
calculated bias is returned from the function that calculates
it and callers use it to fixup their pointers.

Fix fix this with minimal changes to uptream keep the pointer
calculations done by upstream but post-process the resulting
pointers. Post processing calculates the offset of the pointer
within the reallocated region and adds this offset to the base
of the region thus using pointer bounds of the new regions
without any change to the address.

editors/vi.c
@@ -1490,6 +1490,8 @@ static void yank_status(const char *op, const char *p, int cnt)
 static void undo_push(char *, unsigned, int);
 #endif
 
+#define F(x) do { (x) = text + ((x) - text); } while (0)
+
 // open a hole in text[]
 // might reallocate text[]! use p += text_hole_make(p, ...),
 // and be careful to not use pointers into potentially freed text[]!
@@ -1505,16 +1507,18 @@ static uintptr_t text_hole_make(char *p, int size)	// at "p", make a 'size' byte
 		text_size += end - (text + text_size) + 10240;
 		new_text = xrealloc(text, text_size);
 		bias = (new_text - text);
-		screenbegin += bias;
-		dot         += bias;
-		end         += bias;
-		p           += bias;
+		screenbegin += bias; F(screenbegin);
+		dot         += bias; F(dot);
+		end         += bias; F(end);
+		p           += bias; F(p);
 #if ENABLE_FEATURE_VI_YANKMARK
 		{
 			int i;
 			for (i = 0; i < ARRAY_SIZE(mark); i++)
-				if (mark[i])
+				if (mark[i]) {
 					mark[i] += bias;
+					F(mark[i]);
+				}
 		}
 #endif
 		text = new_text;
@@ -2007,7 +2011,7 @@ static int file_insert(const char *fn, char *p, int initial)
 		goto fi;
 	}
 	size = (statbuf.st_size < INT_MAX ? (int)statbuf.st_size : INT_MAX);
-	p += text_hole_make(p, size);
+	p += text_hole_make(p, size); F(p);
 	cnt = full_read(fd, p, size);
 	if (cnt < 0) {
 		status_line_bold_errno(fn);
@@ -2096,7 +2100,7 @@ static uintptr_t stupid_insert(char *p, char c) // stupidly insert the char c at
 {
 	uintptr_t bias;
 	bias = text_hole_make(p, 1);
-	p += bias;
+	p += bias; F(p);
 	*p = c;
 	return bias;
 }
@@ -2125,6 +2129,7 @@ static char *char_insert(char *p, char c, int undo) // insert the char c at 'p'
 
 	if (c == 22) {		// Is this an ctrl-V?
 		p += stupid_insert(p, '^');	// use ^ to indicate literal next
+		F(p);
 		refresh(FALSE);	// show the ^
 		c = get_one_char();
 		*p = c;
@@ -2211,6 +2216,7 @@ static char *char_insert(char *p, char c, int undo) // insert the char c at 'p'
 		modified_count++;
 #endif
 		p += 1 + stupid_insert(p, c);	// insert the char
+		F(p);
 #if ENABLE_FEATURE_VI_SETOPTS
 		if (showmatch && strchr(")]}", c) != NULL) {
 			showmatching(p - 1);
@@ -2248,7 +2254,7 @@ static char *char_insert(char *p, char c, int undo) // insert the char c at 'p'
 					ntab = col / tabstop;
 					nspc = col % tabstop;
 				}
-				p += text_hole_make(p, ntab + nspc);
+				p += text_hole_make(p, ntab + nspc); F(p);
 # if ENABLE_FEATURE_VI_UNDO
 				undo_push_insert(p, ntab + nspc, undo);
 # endif
@@ -2348,7 +2354,7 @@ static uintptr_t string_insert(char *p, const char *s, int undo) // insert the s
 	undo_push_insert(p, i, undo);
 #endif
 	bias = text_hole_make(p, i);
-	p += bias;
+	p += bias; F(p);
 	memcpy(p, s, i);
 	return bias;
 }
@@ -3296,8 +3302,8 @@ static void colon(char *buf)
 				if (len_R != 0) {	// insert the "replace" pattern, if required
 					bias = string_insert(found, R,
 								TEST_UNDO2 ? ALLOW_UNDO_CHAIN : ALLOW_UNDO);
-					found += bias;
-					ls += bias;
+					found += bias; F(found);
+					ls += bias; F(ls);
 					//q += bias; - recalculated anyway
 				}
 #  if ENABLE_FEATURE_VI_REGEX_SEARCH
@@ -3961,6 +3967,7 @@ static void do_cmd(int c)
 			q = end_line(dot);
 			p = text_hole_delete(p, q, ALLOW_UNDO);	// delete cur line
 			p += string_insert(p, reg[Ureg], ALLOW_UNDO_CHAIN);	// insert orig line
+			F(p);
 			dot = p;
 			dot_skip_over_ws();
 # if ENABLE_FEATURE_VI_YANKMARK && ENABLE_FEATURE_VI_VERBOSE_STATUS

commit 1ad1617a01845c67325dea5307fce495ae37a937

vi: use correct base for F after text buffer resize
printf 'a%.s' `seq 1 10300` | vi -

produces a cheri fault.

If no file is loaded at startup, vi reserves an initial text buffer of
10240 bytes. realloc is called to increase this buffer as needed.

After a realloc, all pointers to locations inside the previous buffer are
updated to point to the new buffer. This is done by adding the offset
between the old and new buffers. For cheri, the F macro was introduced to
fix up the capabilities after adding the offset. F recreates the
capabilities with the bounds of the text buffer ("text" points to this
buffer).

When the text buffer itself is moved by the realloc, we have to set
"text" to the new location before calling F. Otherwise, we'll use the
bounds of the old text buffer.

Fixes: e3fa0b3d1 ("Fix pointer handling in busybox's vi")

editors/vi.c
@@ -1511,6 +1511,7 @@ static uintptr_t text_hole_make(char *p, int size)	// at "p", make a 'size' byte
 		text_size += end - (text + text_size) + 10240;
 		new_text = xrealloc(text, text_size);
 		bias = (new_text - text);
+		text = new_text;
 		screenbegin += bias; F(screenbegin);
 		dot         += bias; F(dot);
 		end         += bias; F(end);
@@ -1525,7 +1526,6 @@ static uintptr_t text_hole_make(char *p, int size)	// at "p", make a 'size' byte
 				}
 		}
 #endif
-		text = new_text;
 	}
 	memmove(p + size, p, end - size - p);
 	memset(p, ' ', size);	// clear new hole